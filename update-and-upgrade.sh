f2a7ed751247c8c6edea0a58a81db2dd75915f4d
Amir Sasani <sasani.amir.1998@gmail.com>
2020-12-12 15:16:41 +0330

    add lists for similar distros

--- update-and-upgrade.sh
+++ update-and-upgrade.sh
@@ -1,28 +1,44 @@
 #! /bin/bash
 
+# colors
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NOCOLOR='\033[0m' # No Color
 
+# detet distro name
 DISTRO=$(lsb_release -d | awk -F"\t" '{print $2}' | awk -F " " '{print $1}')
+
+#distro lists
+DEBIAN_BASED="Ubuntu Raspbian"
+ARCH_BASED="Manjaro Arch"
+
+
+#functions
+#------------------------------------------------------------------------------
+isIn() {
+    [[ $2 =~ (^|[[:space:]])$1($|[[:space:]]) ]] && echo 1 || echo 0
+}
+#------------------------------------------------------------------------------
+
 
 echo -e "${GREEN}--------------------------------------------${NOCOLOR}"
 echo -e "            Your distro is ${GREEN}${DISTRO}${NOCOLOR}"
 echo -e "${GREEN}--------------------------------------------${NOCOLOR}"
 
-if [ $DISTRO == 'Manjaro' ]
+if [ $(isIn $DISTRO $ARCH_BASED) == 1 ]
 then
     sudo pacman -Syu --noconfirm;
     sudo pacman -Rns $(pacman -Qdtq) --noconfirm;
     sudo pacman -Sc --noconfirm;
 
-elif [ $DISTRO == 'Ubuntu' ]
+elif [ $(isIn $DISTRO $DEBIAN_BASED) == 1 ]
 then
     sudo apt update -y;
     sudo apt upgrade -y;
     sudo apt autoremove -y;
     sudo apt autoclean -y;
+
 else
     echo -e "${RED}NOT SUPPORTED!${NOCOLOR}\n"
     echo -e "Please contribute in github to make this command better and better :) don't forget to give it a ${YELLOW}STAR${NOCOLOR}\n"
